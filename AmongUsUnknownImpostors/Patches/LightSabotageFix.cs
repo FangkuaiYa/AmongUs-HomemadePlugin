64f670279ff5d794c1d4554b48b3606db0d04a43
MonsoonJiFeng <1767265134@qq.com>
2025-03-02 10:42:37 +0800

    Initial commit

---
+++ AmongUsUnknownImpostors/Patches/LightSabotageFix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AmongUsUnknownImpostors.Patches
+{
+    class LightSabotageFix
+    {
+        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CalculateLightRadius))]
+        public static class ShipStatus_CalculateLightRadius
+        {
+            public static bool Prefix(ShipStatus __instance, GameData.PlayerInfo IIEKJBMPELC, ref float __result)
+            {
+                if (!CustomGameOptionsData.customGameOptions.unkImpostor.value) return true;
+                var player = IIEKJBMPELC;
+                if (player == null || player.IsDead)
+                {
+                    __result = __instance.MaxLightRadius;
+                    return false;
+                }
+
+                SwitchSystem switchSystem = __instance.Systems[SystemTypes.Electrical].Cast<SwitchSystem>();
+                float num = (float) switchSystem.Value / 255f;
+                if (player.IsImpostor)
+                {
+                    __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, num) *
+                               Mathf.Lerp(CustomGameOptionsData.customGameOptions.impoVision.value, PlayerControl.GameOptions.ImpostorLightMod, num);
+                    return false;
+                }
+
+                __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, num) *
+                           PlayerControl.GameOptions.CrewLightMod;
+                return false;
+            }
+        }
+    }
+}
---
+++ AmongUsUnknownImpostors/UnknownImpostorsPlugin.cs
@@ -0,0 +1,27 @@
+﻿using AmongUsUnknownImpostors.Patches;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace AmongUsUnknownImpostors
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    [BepInDependency("com.herysia.LobbyOptionsAPI")]
+    public class UnknownImpostorsPlugin : BasePlugin
+    {
+        public const string Id = "com.herysia.amongusunkimpostor";
+        public static byte rpcSettingsId = 70;
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public override void Load()
+        {
+            CustomGameOptionsData.customGameOptions = new CustomGameOptionsData();
+            Harmony.PatchAll();
+        }
+    }
+}
